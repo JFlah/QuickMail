d7210837eaad44457c776493182ad09a344a46f2
Jack Flaherty <thepacific@me.com>
2016-01-30 17:31:18 -0500

    now traversing text file and emailing everyone in it

--- Mail.java
+++ Mail.java
@@ -1,18 +1,38 @@
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Mail {
     public static void main (String[] args){
+        processFile();
+    }
 
+    public static void processFile() {
+        File file = new File("emails.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String email = sc.nextLine();
+                send(email);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void send(String email){
         try{
             final String fromEmail = ""; //requires valid gmail id
             final String password = ""; // correct password for gmail id
-            final String toEmail = ""; // can be any email id
+            final String toEmail = email; // can be any email id
             final String emailMessage = "hi"; // our message
 
-            System.out.println("OPMail Begin");
+            //System.out.println("OPMail Begin");
             Properties props = new Properties();
             props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
             props.put("mail.smtp.port", "587"); //TLS Port
